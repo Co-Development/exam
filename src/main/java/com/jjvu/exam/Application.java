acb77afc7a2500948c61a3b27b346f8fc346fe01
SongMin90 <13037232106@163.com>
2017-11-27 23:12:25 +0800

    init project

---
+++ src/main/java/com/jjvu/exam/Application.java
@@ -0,0 +1,18 @@
+package com.jjvu.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 系统启动路口
+ * @author SongM
+ * @date 2017年11月27日 下午10:50:22
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
