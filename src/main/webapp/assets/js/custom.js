8b2e4b9f087c26698bc3068914636c487544e3d3
Snailluo <Snail_luo@outlook.com>
2017-12-05 00:36:57 +0800

    倒计时结束自动提交答案

--- src/main/webapp/assets/js/custom.js
+++ src/main/webapp/assets/js/custom.js
@@ -7,7 +7,25 @@ $(function(){
 				_this.html(d.m+":"+d.s);
 			}else{
 				_this.removeClass("on");
-				alert("提示");
+				alert("答题时间到，将自动提交答案。");
+				$("input").attr("disabled");
+				$.ajax({
+					type:'POST',
+					async:false,
+					url:'/examen/testSubmit',
+					data:$('#exam_form').serialize(),
+					dataType:'json',
+					success:function(data){
+						if(data.state == '1') {
+							location.href=data.url;
+						} else {
+							alert('请将试卷完成！');
+						}
+					},
+					error:function(XMLHttpRequest, textStatus, errorThrown){
+						alert(textStatus+XMLHttpRequest.status);  
+					}
+				});
 			}
 		});
 	}
